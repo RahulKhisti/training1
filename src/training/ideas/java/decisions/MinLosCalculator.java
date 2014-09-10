6a9394105e1799b18109235563a7b0f3cc211fda
RahulKhisti <rahul.khisti07@gmail.com>
2014-09-10 22:06:15 +0530

    StringOperations

--- src/training/ideas/java/decisions/MinLosCalculator.java
+++ src/training/ideas/java/decisions/MinLosCalculator.java
@@ -1,10 +1,16 @@
 package training.ideas.java.decisions;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by idnrkh on 7/28/2014.
  */
 public class MinLosCalculator {
-    public static int minlos(String fplosPattern) {
+
+        public  int minlos( String fplosPattern) {
        /* int counter=0;
         for (counter = 0; counter < fplosPattern.length; counter++) {
 
@@ -16,7 +22,37 @@ public class MinLosCalculator {
 
         return 0;*/
 
-       return fplosPattern.toUpperCase().indexOf("Y")+1;
 
+       if(fplosPattern != null)
+       return fplosPattern.toUpperCase().indexOf("Y")+1;
+       else
+       return -1;
     }
-}
+
+    public Map<Date, Map<String, Integer>> minlos( TreeMap<Date,TreeMap<String, String>> rateWisePattern) {
+        Map<Date,Map<String,Integer>>decisionMap = new TreeMap<Date, Map<String, Integer>>();
+
+        Set<Date> occupancyDate = rateWisePattern.keySet();
+
+        for (Date eachDate:occupancyDate){
+            Map<String,String>rateCode=new TreeMap<String, String>();
+            rateCode=rateWisePattern.get(eachDate);
+            decisionMap.put(eachDate,calculate(rateCode));
+
+
+        }
+        return  decisionMap;
+    }
+
+    private Map<String, Integer> calculate(Map<String, String> rateCode) {
+
+        TreeMap<String,Integer> resultMap = new TreeMap<String, Integer>();
+        Set<String> keys = rateCode.keySet();
+        for(String eachKey:keys){
+            resultMap.put(eachKey, minlos(rateCode.get(eachKey)));
+        }
+
+        return resultMap;
+    }
+    }
+
